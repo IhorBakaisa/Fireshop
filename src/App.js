aac94c19791a536ce9701fc3fa17c404fe64ca25
IhorBakaisa <73829374+IhorBakaisa@users.noreply.github.com>
2022-07-17 14:49:14 +0300

    step3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,93 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
+import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      currentItems: [],
+      items: [
+        {
+          id: 1,
+          title: 'Logitech G502',
+          img: 'Mouse1.JPEG',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'gaming_mouse',
+          price: '5399'
+        },
+        {
+          id: 2,
+          title: 'Logitech G703',
+          img: 'Mouse2.JPEG',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'gaming_mouse',
+          price: '3899'
+        },
+        {
+          id: 3,
+          title: 'Logitech G903',
+          img: 'Mouse3.JPEG',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'gaming_mouse',
+          price: '5399'
+        },
+      ],
+      showFullItem: false,
+      fillItem: {}
+    }
+    this.state.currentItems = this.state.items
+    this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header orders={this.state.orders} onDelete={this.deleteOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder} />
+
+        {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem}/>}
+        <Footer />
+      </div>
+    )
+  }
+
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem})
+  }
+
+  chooseCategory(category) {
+    if (category === 'all') {
+      this.setState({currentItems: this.state.items})
+      return 
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
+  }
+
+  deleteOrder(id) {
+    this.setState({ orders: this.state.orders.filter(el => el.id !== id) })
+  }
+
+  addToOrder(item) {
+    let isInArray = false
+    this.state.orders.forEach(el => {
+      if(el.id === item.id)
+        isInArray = true
+    })
+    if(!isInArray) 
+      this.setState({orders: [...this.state.orders, item] })
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+  let summa = 0
+  props.orders.forEach(el => summa += Number.parseFloat(el.price))
+  return (<div>
+    {props.orders.map(el => ( 
+      <Order onDelete={props.onDelete} key={el.id} item={el} /> 
+    ))}
+    <p className='summa'>Сума: {new Intl.NumberFormat().format(summa)}₴</p>
+  </div>)
+}
+
+ const showNothing = () => {
+  return (<div className='empty'>
+    <h2>Товарів немає</h2>
+  </div>)
+ }
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false)
+
   return (
     <header> 
         <div>
@@ -10,6 +31,14 @@ export default function Header() {
                 <li>Контакти</li>
                 <li>Кабінет</li>
             </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+            {cartOpen && (
+              <div className='shop-cart'>
+                {props.orders.length > 0 ?
+                  showOrders(props) : showNothing()}
+              </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
